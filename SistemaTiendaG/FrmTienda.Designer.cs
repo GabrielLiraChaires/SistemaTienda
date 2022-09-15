fa5501e5fbd7bfa7b1a58aed53c0990f0fb9f205
Gabriel Chaires <gabrielchaires01@gmail.com>
2022-09-15 09:29:30 -0500

    Versión inicial del proyecto

---
+++ SistemaTiendaG/FrmTienda.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace SistemaTiendaG
+{
+    partial class FrmTienda
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.lblApaterno = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtIDProducto = new System.Windows.Forms.TextBox();
+            this.lbl_IDUsuario = new System.Windows.Forms.Label();
+            this.dgvProductos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(768, 533);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(135, 54);
+            this.btnSalir.TabIndex = 52;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.panel1.Location = new System.Drawing.Point(429, 517);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(473, 10);
+            this.panel1.TabIndex = 51;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnGuardar.Location = new System.Drawing.Point(619, 533);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(133, 54);
+            this.btnGuardar.TabIndex = 50;
+            this.btnGuardar.Text = "GUARDAR";
+            this.btnGuardar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(563, 130);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(286, 26);
+            this.txtPrecio.TabIndex = 47;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(558, 107);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(57, 20);
+            this.lblPassword.TabIndex = 49;
+            this.lblPassword.Text = "Precio:";
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(15, 139);
+            this.txtDescripcion.Multiline = true;
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(510, 94);
+            this.txtDescripcion.TabIndex = 45;
+            // 
+            // lblApaterno
+            // 
+            this.lblApaterno.AutoSize = true;
+            this.lblApaterno.Location = new System.Drawing.Point(12, 107);
+            this.lblApaterno.Name = "lblApaterno";
+            this.lblApaterno.Size = new System.Drawing.Size(96, 20);
+            this.lblApaterno.TabIndex = 48;
+            this.lblApaterno.Text = "Descripción:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(103, 60);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(801, 26);
+            this.txtNombre.TabIndex = 43;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(11, 63);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(69, 20);
+            this.lblNombre.TabIndex = 46;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // txtIDProducto
+            // 
+            this.txtIDProducto.Location = new System.Drawing.Point(103, 12);
+            this.txtIDProducto.Name = "txtIDProducto";
+            this.txtIDProducto.Size = new System.Drawing.Size(286, 26);
+            this.txtIDProducto.TabIndex = 42;
+            // 
+            // lbl_IDUsuario
+            // 
+            this.lbl_IDUsuario.AutoSize = true;
+            this.lbl_IDUsuario.Location = new System.Drawing.Point(11, 15);
+            this.lbl_IDUsuario.Name = "lbl_IDUsuario";
+            this.lbl_IDUsuario.Size = new System.Drawing.Size(98, 20);
+            this.lbl_IDUsuario.TabIndex = 44;
+            this.lbl_IDUsuario.Text = "ID Producto:";
+            // 
+            // dgvProductos
+            // 
+            this.dgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProductos.Location = new System.Drawing.Point(10, 251);
+            this.dgvProductos.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dgvProductos.Name = "dgvProductos";
+            this.dgvProductos.Size = new System.Drawing.Size(892, 258);
+            this.dgvProductos.TabIndex = 41;
+            // 
+            // FrmTienda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(910, 595);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.lblApaterno);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.txtIDProducto);
+            this.Controls.Add(this.lbl_IDUsuario);
+            this.Controls.Add(this.dgvProductos);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FrmTienda";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.Label lblApaterno;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtIDProducto;
+        private System.Windows.Forms.Label lbl_IDUsuario;
+        private System.Windows.Forms.DataGridView dgvProductos;
+    }
+}
+
