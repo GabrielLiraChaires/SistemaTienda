f23e0508e4bfcc82437f45f5101ae912b105fb2e
Gabriel Chaires <gabrielchaires01@gmail.com>
2022-09-15 13:34:03 -0500

    Se modifico la clase ProductoAccesoDatos

--- AccesoDatos.SistemaTiendaG/Conexion.cs
+++ AccesoDatos.SistemaTiendaG/Conexion.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using System.Data;
+using MySql.Data.MySqlClient;
 namespace AccesoDatos.SistemaTiendaG
 {
     public class Conexion
     {
+        private MySqlConnection _conn;
+        public Conexion(string servidor, string usuario, string password, string basedatos, uint puerto)
+        {
+            MySqlConnectionStringBuilder cadenaConexion = new MySqlConnectionStringBuilder();
+            cadenaConexion.Server = servidor;
+            cadenaConexion.UserID = usuario;
+            cadenaConexion.Password = password;
+            cadenaConexion.Database = basedatos;
+            cadenaConexion.Port = puerto;
+
+            _conn = new MySqlConnection(cadenaConexion.ToString());
+        }
+        public void EjecutarConsulta(string consulta)
+        {
+            _conn.Open();
+            var command = new MySqlCommand(consulta, _conn);
+            command.ExecuteNonQuery();
+            _conn.Close();
+        }
+        public DataSet ObtenerDatos(string consulta, string tabla)
+        {
+            var ds = new DataSet();
+            MySqlDataAdapter da = new MySqlDataAdapter(consulta, _conn);
+            da.Fill(ds, tabla);
+            return ds;
+        }
     }
 }
