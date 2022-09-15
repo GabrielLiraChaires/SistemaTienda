d33b6c0ba1ca623a6daf7de4333792ce6a92dd02
Gabriel Chaires <gabrielchaires01@gmail.com>
2022-09-15 13:44:16 -0500

    Se modifico la clase ProductosManejador

--- LogicaNegocio.SistemaTiendaG/ProductosManejador.cs
+++ LogicaNegocio.SistemaTiendaG/ProductosManejador.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using System.Collections.Generic;
+using AccesoDatos.SistemaTiendaG;
+using Entidades.SistemaTiendaG;
 namespace LogicaNegocio.SistemaTiendaG
 {
     public class ProductosManejador
     {
+        private ProductosAccesoDatos _productosAccesoDatos = new ProductosAccesoDatos();
+        public List<Productos> GetProductos(string dato)
+        {
+            var listProductos = _productosAccesoDatos.GetProductos(dato);
+            return listProductos;
+        }
+        public void GuardarProductos(Productos producto)
+        {
+            _productosAccesoDatos.GuardarProductos(producto);
+        }
     }
 }
